37f5de339bcff00f766671dbcfa4f8b704a53c30
michalmelis <michalmelis.sk@gmail.com>
2019-10-09 18:16:48 +0200

    open_lab_01.3

---
+++ open-lab-01.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace open_lab_01._3
+{
+    class Program
+    {
+        public static int Addition(int number)
+        {
+            return (number + 1);
+        }
+
+    }
+}        
+        
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
